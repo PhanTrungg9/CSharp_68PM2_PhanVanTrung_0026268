e90636757d8095db44a01fe889bce977ca0058ee
Phan Văn Trung <vantrung16701@gmail.com>
2026-06-12 10:32:38 +0700

    feat<lophoc>: CRUD lophoc

--- QLSinhvien/UCQL_lophoc.Designer.cs
+++ QLSinhvien/UCQL_lophoc.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_ghichu = new System.Windows.Forms.TextBox();
+            this.txt_tenlop = new System.Windows.Forms.TextBox();
+            this.txt_malop = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_reload = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
@@ -51,17 +51,17 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
-            this.button10 = new System.Windows.Forms.Button();
+            this.btn_listSV = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_QLLH)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox5);
-            this.groupBox1.Controls.Add(this.textBox4);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txt_ghichu);
+            this.groupBox1.Controls.Add(this.txt_tenlop);
+            this.groupBox1.Controls.Add(this.txt_malop);
+            this.groupBox1.Controls.Add(this.txt_id);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
@@ -77,37 +77,37 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin lớp học";
             // 
-            // textBox5
+            // txt_ghichu
             // 
-            this.textBox5.Location = new System.Drawing.Point(7, 303);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(379, 24);
-            this.textBox5.TabIndex = 6;
+            this.txt_ghichu.Location = new System.Drawing.Point(7, 303);
+            this.txt_ghichu.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_ghichu.Name = "txt_ghichu";
+            this.txt_ghichu.Size = new System.Drawing.Size(379, 24);
+            this.txt_ghichu.TabIndex = 6;
             // 
-            // textBox4
+            // txt_tenlop
             // 
-            this.textBox4.Location = new System.Drawing.Point(10, 219);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(379, 24);
-            this.textBox4.TabIndex = 6;
+            this.txt_tenlop.Location = new System.Drawing.Point(10, 219);
+            this.txt_tenlop.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_tenlop.Name = "txt_tenlop";
+            this.txt_tenlop.Size = new System.Drawing.Size(379, 24);
+            this.txt_tenlop.TabIndex = 6;
             // 
-            // textBox2
+            // txt_malop
             // 
-            this.textBox2.Location = new System.Drawing.Point(10, 144);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(379, 24);
-            this.textBox2.TabIndex = 6;
+            this.txt_malop.Location = new System.Drawing.Point(10, 144);
+            this.txt_malop.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_malop.Name = "txt_malop";
+            this.txt_malop.Size = new System.Drawing.Size(379, 24);
+            this.txt_malop.TabIndex = 6;
             // 
-            // textBox1
+            // txt_id
             // 
-            this.textBox1.Location = new System.Drawing.Point(10, 68);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(379, 24);
-            this.textBox1.TabIndex = 5;
+            this.txt_id.Location = new System.Drawing.Point(10, 68);
+            this.txt_id.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(379, 24);
+            this.txt_id.TabIndex = 5;
             // 
             // label5
             // 
@@ -163,53 +163,53 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã ID:";
             // 
-            // button1
+            // btn_add
             // 
-            this.button1.AllowDrop = true;
-            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(5, 441);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 50);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Thêm";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_add.AllowDrop = true;
+            this.btn_add.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_add.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_add.Location = new System.Drawing.Point(5, 441);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(184, 50);
+            this.btn_add.TabIndex = 2;
+            this.btn_add.Text = "Thêm";
+            this.btn_add.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // btn_update
             // 
-            this.button2.BackColor = System.Drawing.Color.LimeGreen;
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(227, 441);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(174, 50);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Sửa";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btn_update.BackColor = System.Drawing.Color.LimeGreen;
+            this.btn_update.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_update.Location = new System.Drawing.Point(227, 441);
+            this.btn_update.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(174, 50);
+            this.btn_update.TabIndex = 3;
+            this.btn_update.Text = "Sửa";
+            this.btn_update.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // btn_delete
             // 
-            this.button3.BackColor = System.Drawing.Color.Red;
-            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button3.Location = new System.Drawing.Point(5, 509);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(184, 49);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Xóa";
-            this.button3.UseVisualStyleBackColor = false;
+            this.btn_delete.BackColor = System.Drawing.Color.Red;
+            this.btn_delete.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_delete.Location = new System.Drawing.Point(5, 509);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(184, 49);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "Xóa";
+            this.btn_delete.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // btn_reload
             // 
-            this.button4.BackColor = System.Drawing.Color.Silver;
-            this.button4.Location = new System.Drawing.Point(227, 509);
-            this.button4.Margin = new System.Windows.Forms.Padding(2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(174, 49);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Làm mới";
-            this.button4.UseVisualStyleBackColor = false;
+            this.btn_reload.BackColor = System.Drawing.Color.Silver;
+            this.btn_reload.Location = new System.Drawing.Point(227, 509);
+            this.btn_reload.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_reload.Name = "btn_reload";
+            this.btn_reload.Size = new System.Drawing.Size(174, 49);
+            this.btn_reload.TabIndex = 5;
+            this.btn_reload.Text = "Làm mới";
+            this.btn_reload.UseVisualStyleBackColor = false;
             // 
             // label6
             // 
@@ -306,18 +306,18 @@
             this.label7.TabIndex = 14;
             this.label7.Text = "Trang 1/1 | 3 bản ghi";
             // 
-            // button10
+            // btn_listSV
             // 
-            this.button10.AllowDrop = true;
-            this.button10.BackColor = System.Drawing.Color.RoyalBlue;
-            this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button10.Location = new System.Drawing.Point(5, 581);
-            this.button10.Margin = new System.Windows.Forms.Padding(2);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(396, 50);
-            this.button10.TabIndex = 2;
-            this.button10.Text = "Xem danh sách sinh viên";
-            this.button10.UseVisualStyleBackColor = false;
+            this.btn_listSV.AllowDrop = true;
+            this.btn_listSV.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btn_listSV.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_listSV.Location = new System.Drawing.Point(5, 581);
+            this.btn_listSV.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_listSV.Name = "btn_listSV";
+            this.btn_listSV.Size = new System.Drawing.Size(396, 50);
+            this.btn_listSV.TabIndex = 2;
+            this.btn_listSV.Text = "Xem danh sách sinh viên";
+            this.btn_listSV.UseVisualStyleBackColor = false;
             // 
             // UCQL_lophoc
             // 
@@ -332,11 +332,11 @@
             this.Controls.Add(this.button5);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_reload);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_listSV);
+            this.Controls.Add(this.btn_add);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5);
@@ -358,12 +358,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txt_malop;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_reload;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button5;
@@ -373,8 +373,8 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox txt_ghichu;
+        private System.Windows.Forms.TextBox txt_tenlop;
+        private System.Windows.Forms.Button btn_listSV;
     }
 }
--- QLSinhvien/UCQL_lophoc.cs
+++ QLSinhvien/UCQL_lophoc.cs
@@ -7,22 +7,428 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace QLSinhvien
 {
     public partial class UCQL_lophoc : UserControl
     {
         DatabaseDataContext db = new DatabaseDataContext();
+
+        int currentPage = 1;
+        int pageSize = 5;
+        int totalRecords = 0;
+        int totalPages = 1;
+        string keyword = "";
+
         public UCQL_lophoc()
         {
             InitializeComponent();
+
+            dgv_QLLH.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv_QLLH.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgv_QLLH.CellClick += dgv_QLLH_CellClick;
+
+            btn_add.Click += btn_add_Click;
+            btn_update.Click += btn_update_Click;
+            btn_delete.Click += btn_delete_Click;
+            btn_reload.Click += btn_reload_Click;
+            btn_listSV.Click += btn_listSV_Click;
+
+            button5.Click += button5_Click;
+
+            button6.Click += button6_Click; // <<
+            button7.Click += button7_Click; // <
+            button9.Click += button9_Click; // >
+            button8.Click += button8_Click; // >>
         }
 
         private void UCQL_lophoc_Load(object sender, EventArgs e)
         {
-            List<Lop> ds = db.Lops.ToList();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var query = db.Lops.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                int idSearch;
+                bool isNumber = int.TryParse(keyword, out idSearch);
+
+                query = query.Where(l =>
+                    l.malop.Contains(keyword) ||
+                    l.tenlop.Contains(keyword) ||
+                    (isNumber && l.id == idSearch)
+                );
+            }
+
+            totalRecords = query.Count();
+
+            totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+            if (totalPages == 0)
+            {
+                totalPages = 1;
+            }
+
+            if (currentPage < 1)
+            {
+                currentPage = 1;
+            }
+
+            if (currentPage > totalPages)
+            {
+                currentPage = totalPages;
+            }
+
+            var ds = query
+                .OrderBy(l => l.id)
+                .Skip((currentPage - 1) * pageSize)
+                .Take(pageSize)
+                .Select(l => new
+                {
+                    l.id,
+                    l.malop,
+                    l.tenlop,
+                    l.ghichu
+                })
+                .ToList();
+
             dgv_QLLH.DataSource = ds;
-            dgv_QLLH.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                label7.Text = $"Trang {currentPage}/{totalPages} | {totalRecords} bản ghi";
+            }
+            else
+            {
+                label7.Text = $"Trang {currentPage}/{totalPages} | Tìm thấy {totalRecords} bản ghi";
+            }
+
+            button6.Enabled = currentPage > 1;
+            button7.Enabled = currentPage > 1;
+            button9.Enabled = currentPage < totalPages;
+            button8.Enabled = currentPage < totalPages;
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_id.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng nhập mã ID!");
+                    return;
+                }
+
+                if (txt_malop.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng nhập mã lớp!");
+                    return;
+                }
+
+                if (txt_tenlop.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng nhập tên lớp!");
+                    return;
+                }
+
+                int id;
+
+                if (!int.TryParse(txt_id.Text.Trim(), out id))
+                {
+                    MessageBox.Show("Mã ID phải là số!");
+                    return;
+                }
+
+                Lop checkId = db.Lops.FirstOrDefault(l => l.id == id);
+
+                if (checkId != null)
+                {
+                    MessageBox.Show("Mã ID này đã tồn tại!");
+                    return;
+                }
+
+                Lop checkMaLop = db.Lops.FirstOrDefault(l => l.malop == txt_malop.Text.Trim());
+
+                if (checkMaLop != null)
+                {
+                    MessageBox.Show("Mã lớp này đã tồn tại!");
+                    return;
+                }
+
+                Lop lop = new Lop();
+
+                lop.id = id;
+                lop.malop = txt_malop.Text.Trim();
+                lop.tenlop = txt_tenlop.Text.Trim();
+                lop.ghichu = txt_ghichu.Text.Trim();
+
+                db.Lops.InsertOnSubmit(lop);
+                db.SubmitChanges();
+
+                totalRecords = db.Lops.Count();
+                totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+                currentPage = totalPages;
+
+                keyword = "";
+                textBox3.Clear();
+
+                MessageBox.Show("Thêm lớp học thành công!");
+
+                LoadData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi thêm: " + ex.Message);
+            }
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_id.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng chọn lớp học cần sửa!");
+                    return;
+                }
+
+                int id = Convert.ToInt32(txt_id.Text.Trim());
+
+                Lop lop = db.Lops.FirstOrDefault(l => l.id == id);
+
+                if (lop == null)
+                {
+                    MessageBox.Show("Không tìm thấy lớp học cần sửa!");
+                    return;
+                }
+
+                if (txt_malop.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng nhập mã lớp!");
+                    return;
+                }
+
+                if (txt_tenlop.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng nhập tên lớp!");
+                    return;
+                }
+
+                Lop checkMaLop = db.Lops.FirstOrDefault(l =>
+                    l.malop == txt_malop.Text.Trim() && l.id != id
+                );
+
+                if (checkMaLop != null)
+                {
+                    MessageBox.Show("Mã lớp này đã tồn tại ở lớp khác!");
+                    return;
+                }
+
+                lop.malop = txt_malop.Text.Trim();
+                lop.tenlop = txt_tenlop.Text.Trim();
+                lop.ghichu = txt_ghichu.Text.Trim();
+
+                db.SubmitChanges();
+
+                MessageBox.Show("Sửa lớp học thành công!");
+
+                LoadData();
+                ClearForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi sửa: " + ex.Message);
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_id.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng chọn lớp học cần xóa!");
+                    return;
+                }
+
+                int id = Convert.ToInt32(txt_id.Text.Trim());
+
+                Lop lop = db.Lops.FirstOrDefault(l => l.id == id);
+
+                if (lop == null)
+                {
+                    MessageBox.Show("Không tìm thấy lớp học cần xóa!");
+                    return;
+                }
+
+                int soSinhVien = db.SinhViens.Count(sv => sv.malop == id);
+
+                if (soSinhVien > 0)
+                {
+                    MessageBox.Show("Không thể xóa lớp này vì đang có sinh viên thuộc lớp này!");
+                    return;
+                }
+
+                DialogResult result = MessageBox.Show(
+                    "Bạn có chắc muốn xóa lớp học này không?",
+                    "Xác nhận xóa",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    db.Lops.DeleteOnSubmit(lop);
+                    db.SubmitChanges();
+
+                    MessageBox.Show("Xóa lớp học thành công!");
+
+                    LoadData();
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xóa: " + ex.Message);
+            }
+        }
+
+        private void btn_reload_Click(object sender, EventArgs e)
+        {
+            keyword = "";
+            textBox3.Clear();
+
+            currentPage = 1;
+
+            LoadData();
+            ClearForm();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            keyword = textBox3.Text.Trim();
+
+            if (keyword == "")
+            {
+                MessageBox.Show("Vui lòng nhập từ khóa cần tìm!");
+                return;
+            }
+
+            currentPage = 1;
+            LoadData();
+
+            if (totalRecords == 0)
+            {
+                MessageBox.Show("Không tìm thấy lớp học phù hợp!");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            currentPage = 1;
+            LoadData();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                LoadData();
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (currentPage < totalPages)
+            {
+                currentPage++;
+                LoadData();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            currentPage = totalPages;
+            LoadData();
+        }
+
+        private void dgv_QLLH_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgv_QLLH.Rows[e.RowIndex];
+
+                txt_id.Text = row.Cells["id"].Value.ToString();
+                txt_malop.Text = row.Cells["malop"].Value.ToString();
+                txt_tenlop.Text = row.Cells["tenlop"].Value.ToString();
+
+                if (row.Cells["ghichu"].Value != null)
+                {
+                    txt_ghichu.Text = row.Cells["ghichu"].Value.ToString();
+                }
+                else
+                {
+                    txt_ghichu.Text = "";
+                }
+
+                txt_id.ReadOnly = true;
+            }
+        }
+
+        private void ClearForm()
+        {
+            txt_id.Clear();
+            txt_malop.Clear();
+            txt_tenlop.Clear();
+            txt_ghichu.Clear();
+
+            txt_id.ReadOnly = false;
+        }
+
+        private void btn_listSV_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_id.Text.Trim() == "")
+                {
+                    MessageBox.Show("Vui lòng chọn lớp học để xem danh sách sinh viên!");
+                    return;
+                }
+
+                int idLop = Convert.ToInt32(txt_id.Text.Trim());
+
+                var dsSinhVien = db.SinhViens
+                    .Where(sv => sv.malop == idLop)
+                    .Select(sv => new
+                    {
+                        sv.id,
+                        sv.hoten,
+                        sv.gioitinh,
+                        sv.ngaysinh,
+                        sv.malop
+                    })
+                    .ToList();
+
+                dgv_QLLH.DataSource = dsSinhVien;
+
+                label7.Text = $"Danh sách sinh viên | {dsSinhVien.Count} bản ghi";
+
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xem danh sách sinh viên: " + ex.Message);
+            }
         }
     }
 }
